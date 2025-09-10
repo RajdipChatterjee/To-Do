1773b59dbf162656dd7ee612fa17fc339bac7633
RajdipChatterjee <rajdipchatterjee3000@gmail.com>
2025-09-11 01:18:19 +0530

    Add and Delete Task functionality and re-rendering on task save and deletion is added

--- script.js
+++ script.js
@@ -4,31 +4,43 @@ document.addEventListener("DOMContentLoaded", () => {
   let todoList = document.getElementById("todo-list");
 
   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
+  
   tasks.forEach(task => renderTask(task));
   addTaskButton.addEventListener("click", () => {
-    const taskText = todoInput.value.trim();
-    if (taskText === "") return;
-
+    let taskText = todoInput.value.trim();
+    if (taskText == "") return;
     let newTask = {
       id: Date.now(),
-      text: todoInput.value,
+      text: taskText,
       completed: false,
     };
     tasks.push(newTask);
-    saveTask();
+    saveTasks();
+    renderTask(newTask);
     todoInput.value = ""; // clear input
   });
 
   function renderTask(task) {
     const li = document.createElement('li');
     li.setAttribute('data-id', task.id)
-    li.innerHTML = `${task.text} <button>delete</button></>`
+    li.innerHTML = `<span>${task.text}</span><button>delete</button>`
     todoList.appendChild(li);
+    li.addEventListener('click', (e) => {
+      if(e.target.tagName === 'BUTTON') return;
+      task.completed = !task.completed;
+      li.classList.toggle('completed')
+      saveTasks();
+    })
+
+    li.querySelector('button').addEventListener('click', (e) => {
+      e.stopPropagation(); // prevent toggle from firing
+      tasks = tasks.filter((t) => t.id != task.id);
+      li.remove();
+      saveTasks();
+    })
   }
 
-  function saveTask() {
+  function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    renderTask();
   }
 });
